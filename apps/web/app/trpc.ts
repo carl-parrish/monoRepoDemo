8530cf40158ff41bbb5e3d7aec4e978116ada4a9
Carl Parrish <carl.parrish@gmail.com>
2024-01-10 14:23:53 -0700

    chore: update next version

--- apps/web/app/trpc.ts
+++ apps/web/app/trpc.ts
@@ -1,12 +1,11 @@
-import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
-import { AppRouter } from '@server/trpc/trpc.router';
+import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
+import { AppRouter } from "@server/trpc/trpc.router";
 
-export const trpc =  createTRPCProxyClient<AppRouter>({
-    links: [
-        httpBatchLink({
-            url: `${process.env.NEXT_PUBLIC_NESTJS_SERVER}/trpc`,
-        }),
-    ],
+export const trpc = createTRPCProxyClient<AppRouter>({
+  links: [
+    httpBatchLink({
+      //url: `${process.env.NEXT_PUBLIC_NESTJS_SERVER}/trpc`,
+      url: `http://localhost:3000/trpc`,
+    }),
+  ],
 });
-
-
